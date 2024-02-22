37871a87c18e30487320130e94190ac63f7b0311
rena <rena21792@gmail.com>
2024-02-22 17:21:21 +0530

    registration

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:festive_fusion/registration.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -18,60 +19,83 @@ class _LoginState extends State<Login> {
             width: 300,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-            
               children: [
-                Text('LOGIN',style: TextStyle(color: const Color.fromARGB(255, 163, 33, 185),fontSize: 20,
-                                  fontWeight: FontWeight.bold),),
-                                  SizedBox(height:50,),
-                                
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.all(12.0),
-                                        child: Text('EMAIL'),
-                                      ),
-                                    ],
-                                  ),
-                                  TextFormField(decoration: InputDecoration
-                                  (fillColor: Color.fromARGB(255, 224, 206, 221),filled:true,
-                                    border: OutlineInputBorder
-                                  (borderRadius: BorderRadius.all(Radius.circular(40))),
-                                  hintText: ('ENTER EMAIL')),
-                                  ),
-                                  SizedBox(height: 30,),
-                                  Container(child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.all(12.0),
-                                        child: Text('PASSWORD'),
-                                      ),
-                                    ],
-                                  )),
-                                  TextFormField(
-                                    decoration: InputDecoration(fillColor:Color.fromARGB(255, 224, 206, 221),filled:true,border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.all(Radius.circular(40))
-                                    ),
-                                    hintText: ('ENTER YOUR PASSWORD')),
-                                    
-            
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      Text("forgot password?"),
-                                    ],
-                                  ),
-                                  SizedBox(height: 50,),
-                                  ElevatedButton(onPressed: (){}, child: Text('LOGIN')),
-                                  TextButton(onPressed: (){}, child: Text('Not Registered yet?Sign up'))
+                Text(
+                  'Festive Fusion',
+                  style: TextStyle(
+                      fontFamily: 'irish grover',
+                      color: const Color.fromARGB(255, 163, 33, 185),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Row(
+
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text('EMAIL'),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      fillColor: Color.fromARGB(255, 224, 206, 221),
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      hintText: ('ENTER EMAIL')),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Text('PASSWORD'),
+                    ),
+                  ],
+                )),
+                TextFormField(
+                  decoration: InputDecoration(
+                      fillColor: Color.fromARGB(255, 224, 206, 221),
+                      filled: true,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      hintText: ('ENTER YOUR PASSWORD')),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("forgot password?",style: TextStyle(decoration: TextDecoration.underline,color: Color.fromARGB(255, 20, 97, 160)),),
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(onPressed: () {}, 
+                style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple)),
+                child: Text('LOGIN',style: TextStyle(color: Color.fromARGB(255, 245, 244, 245)),)),
+                SizedBox(
+                  height: 30,
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder:(context) {
+                        return Registration();
+                      },));
+                    }, child: Text('Not Registered yet?Sign up'))
               ],
             ),
           ),
         ],
       ),
-                        
     );
   }
 }
---
+++ lib/registration.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+
+class Registration extends StatefulWidget {
+  const Registration({super.key});
+
+  @override
+  State<Registration> createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  String gender="";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20,bottom: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(width: 300,
+                child: SingleChildScrollView(
+                  
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 50,width: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: const Color.fromARGB(255, 154, 134, 189)
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Row(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text('Name'),
+                            ),
+                          ],
+                        ),
+                        TextFormField(decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                        SizedBox(height: 20,),
+                        Row(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text('Email'),
+                            ),
+                          ],
+                        ),
+                        TextFormField(decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                        Row(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text('Gender'),
+                            ),
+                          ],
+                        ),
+                        RadioListTile(
+                          title: Text('Male'),
+                        
+                        value: 'Male', groupValue: gender, onChanged: (value){
+                          setState(() {
+                            gender=value.toString();
+                          });
+                        }),
+                        RadioListTile(
+                          title: Text('Female'),
+                        
+                        value: 'Female', groupValue: gender, onChanged: (value){
+                          setState(() {
+                            gender=value.toString();
+                          });
+                        }),
+                        RadioListTile(
+                          title: Text('Others'),
+                        
+                        value: 'others', groupValue: gender, onChanged: (value){
+                          setState(() {
+                            gender=value.toString();
+                          });
+                        }),
+                         Row(mainAxisAlignment: MainAxisAlignment.start,
+                           children: [
+                             Padding(
+                               padding: const EdgeInsets.all(12.0),
+                               child: Text('House name'),
+                             ),
+                           ],
+                         ),
+                        TextFormField(decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                         Row(mainAxisAlignment: MainAxisAlignment.start,
+                           children: [
+                             Padding(
+                               padding: const EdgeInsets.all(12.0),
+                               child: Text('state'),
+                             ),
+                           ],
+                         ),
+                        TextFormField(decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                         Row(mainAxisAlignment: MainAxisAlignment.start,
+                           children: [
+                             Padding(
+                               padding: const EdgeInsets.all(12.0),
+                               child: Text('District'),
+                             ),
+                           ],
+                         ),
+                        TextFormField(decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                        Row(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text('Pin No'),
+                            ),
+                          ],
+                        ),
+                        TextFormField(keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                        Row(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text('Mobile Number'),
+                            ),
+                          ],
+                        ),
+                        TextFormField(keyboardType: TextInputType.phone,
+                          decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                        Row(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text('Password'),
+                            ),
+                          ],
+                        ),
+                        TextFormField(decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                        Row(mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text('confirm password'),
+                            ),
+                          ],
+                        ),
+                        TextFormField(decoration: InputDecoration(fillColor: Color.fromARGB(255, 224, 206, 221),
+                        filled: true,
+                        border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40)),
+                          borderSide: BorderSide.none)
+                        
+                          
+                        ),),
+                        SizedBox(height: 50,),
+                        ElevatedButton(onPressed: () {}, child: Text('REGISTOR')),
+                    
+                      ],
+                    ),
+                  ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      );
+  }
+}
